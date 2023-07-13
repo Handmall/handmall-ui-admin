9f8f695b797aa9292c6e8bd596ff784e155b6537
rustam <rustamm.aliyev@gmail.com>
2023-07-13 23:04:31 +0400

    update sidebar component for active menu display

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,55 +1,64 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import menuItems from "@constants/menu";
-import { Layout, Menu, MenuProps } from "antd"
-import { createElement, useState } from "react";
+import { Layout, Menu, MenuProps } from "antd";
+import { createElement, useEffect, useState } from "react";
 
-const {  Sider } = Layout
+const { Sider } = Layout;
 
 const Sidebar = () => {
-	const [collapsed, setCollapsed] = useState(false)
+	const currentLocation = useLocation().pathname;
+	const [pathId, setPathId] = useState(0);
+	const [collapsed, setCollapsed] = useState(false);
 
-	const menu: MenuProps['items'] = menuItems.map((item, index) => {
+	const menu: MenuProps["items"] = menuItems.map((item, index) => {
 		return {
 			key: index,
 			icon: createElement(item.icon),
 			label: item.subItems ? (
-                item.name
-            ) : (
-                <Link to={item.url}>{item.name}</Link>
-            ),
+				item.name
+			) : (
+				<Link to={item.url}>{item.name}</Link>
+			),
 			children: item.subItems
-                ? item.subItems.map((si, i) => {
-                      return {
-                          key: (i + 10) * (index + 20) + 10,
-                          icon: createElement(si.icon),
-                          label: <Link to={si.url}>{si.name}</Link>,
-                      }
-                  })
-                : null,
-		}
-	})
+				? item.subItems.map((si, i) => {
+						return {
+							key: (i + 10) * (index + 20) + 10,
+							icon: createElement(si.icon),
+							label: <Link to={si.url}>{si.name}</Link>,
+						};
+				  })
+				: null,
+		};
+	});
 
-    return (
-        <Sider 
+	useEffect(() => {
+		menuItems.forEach((item, index) => {
+			item.url === currentLocation ? setPathId(index) : null;
+		});
+	}, [currentLocation]);
+
+	return (
+		<Sider
 			collapsible
 			collapsed={collapsed}
 			onCollapse={(value: boolean) => setCollapsed(value)}
 		>
 			<div
-                style={{
-                    height: 32,
-                    margin: 16,
-                    background: 'rgba(255, 255, 255, 0.2)',
-                }}
-            />
-			<Menu 
+				style={{
+					height: 32,
+					margin: 16,
+					background: "rgba(255, 255, 255, 0.2)",
+				}}
+			/>
+			<Menu
 				theme="dark"
 				mode="inline"
-				defaultSelectedKeys={['0']}
+				defaultSelectedKeys={[pathId.toString()]}
+				selectedKeys={[pathId.toString()]}
 				items={menu}
 			/>
 		</Sider>
-    )
-}
+	);
+};
 
-export default Sidebar
+export default Sidebar;
