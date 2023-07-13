cc2a9cab1e7ff2ee575da163df059415f485fab8
rustam <rustamm.aliyev@gmail.com>
2023-07-13 23:09:01 +0400

    add changes in redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,14 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { Middleware, configureStore } from "@reduxjs/toolkit";
 import { categoryApi } from "@/services/category.service";
 import userReducer from "@store/slices/userSlice";
 import { authApi } from "@/services/auth.service";
 import { departmentApi } from "@/services/department.service";
+
+const customMiddleware: Middleware[] = [
+    authApi.middleware,
+    categoryApi.middleware,
+    departmentApi.middleware,
+];
 
 export const store = configureStore({
     reducer: {
@@ -12,11 +18,7 @@ export const store = configureStore({
         [departmentApi.reducerPath]: departmentApi.reducer,
     },
     middleware: (getDefaultMiddleware) =>
-        getDefaultMiddleware().concat([
-            authApi.middleware,
-            categoryApi.middleware,
-            departmentApi.middleware,
-        ]),
+        getDefaultMiddleware().concat(customMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
