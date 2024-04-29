120e6f6cd3864e53e69f986307447e9229af7623
rustam <rustamm.aliyev@gmail.com>
2024-04-29 18:47:19 +0400

    changes create method

--- src/modules/Category/create.tsx
+++ src/modules/Category/create.tsx
@@ -1,70 +1,130 @@
-import { Button, Form, Input, Select, Space } from "antd";
+import categoryService from "@/services/category.service.ts";
+import departmentService from "@/services/department.service.ts";
+import { CategoryRequest } from "@/types/category/CategoryRequest.ts";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { Alert, Button, Form, Input, message, Select, Space } from "antd";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const { Option } = Select;
 
 const CategoryCreate = () => {
 	const navigate = useNavigate();
-	const { data: departments } = useGetDepartmentsQuery(1);
-	const createCategory = () => {
-		console.log("create");
+	const [messageApi, contextHolder] = message.useMessage();
+
+	const success = () => {
+		messageApi.open({
+			type: "success",
+			content: "Category added",
+		});
+		setTimeout(messageApi.destroy, 2000);
 	};
 
+	const fetchDepartments = async () => {
+		const { data } = await departmentService.getAll();
+		return data;
+	};
+
+	const { data: departments } = useQuery({
+		queryKey: ["departments"],
+		queryFn: () => fetchDepartments(),
+	});
+
+	const mutation = useMutation({
+		mutationFn: (newCategory: CategoryRequest) =>
+			categoryService.addNew(newCategory),
+	});
+
+	const createCategory = (values: CategoryRequest) => {
+		mutation.mutate(values);
+	};
+
+	useEffect(() => {
+		if (mutation.isSuccess) {
+			success();
+			setTimeout(() => {
+				navigate("/admin/category");
+			}, 2500);
+		}
+	}, [mutation]);
+
 	return (
-		<Space direction="vertical" style={{ display: "flex" }} size={"large"}>
-			<h1 style={{ textAlign: "center" }}>Create a category</h1>
-			<Form
-				name="category"
-				labelCol={{ span: 8 }}
-				wrapperCol={{ span: 16 }}
-				style={{ maxWidth: 600 }}
-				onFinish={createCategory}
+		<>
+			{contextHolder}
+			<Space
+				direction="vertical"
+				style={{ display: "flex" }}
+				size={"large"}
 			>
-				<Form.Item
-					label="Category name"
-					name="name"
-					rules={[
-						{
-							required: true,
-							message: "Please input category name!",
-						},
-					]}
-				>
-					<Input />
-				</Form.Item>
-				<Form.Item label="Category description" name="description">
-					<Input />
-				</Form.Item>
-				{departments ? (
-					<Form.Item label="Department" name="departmentId">
-						<Select
-							showSearch
-							placeholder="Select a department"
-							onChange={(value, options) => console.log()}
-						>
-							{departments.map((department, index) => (
-								<Option key={index} value={department.id}>
-									{department.name}
-								</Option>
-							))}
-						</Select>
-					</Form.Item>
-				) : null}
-				<Form.Item>
-					<Space
+				{mutation.isError ? (
+					<Alert
+						message={`Error: ${mutation.error}`}
+						type="warning"
+						closable
 						style={{
-							display: "flex",
+							marginBottom: "20px",
 						}}
-						size={"large"}
+					/>
+				) : null}
+				<h1 style={{ textAlign: "center" }}>Create a category</h1>
+				<Form
+					name="category"
+					labelCol={{ span: 8 }}
+					wrapperCol={{ span: 16 }}
+					style={{ maxWidth: 600 }}
+					onFinish={createCategory}
+				>
+					<Form.Item
+						label="Category name"
+						name="name"
+						rules={[
+							{
+								required: true,
+								message: "Please input category name!",
+							},
+						]}
 					>
-						<Button onClick={() => navigate(-1)}>Go back</Button>
-						<Button type="primary" htmlType="submit">
-							Create
-						</Button>
-					</Space>
-				</Form.Item>
-			</Form>
-		</Space>
+						<Input />
+					</Form.Item>
+					<Form.Item label="Category description" name="description">
+						<Input />
+					</Form.Item>
+					{departments ? (
+						<Form.Item label="Department" name="departmentId">
+							<Select
+								showSearch
+								placeholder="Select a department"
+							>
+								{departments.map((department, index) => (
+									<Option key={index} value={department.id}>
+										{department.name}
+									</Option>
+								))}
+							</Select>
+						</Form.Item>
+					) : null}
+					<Form.Item>
+						<Space
+							style={{
+								display: "flex",
+							}}
+							size={"large"}
+						>
+							<Button onClick={() => navigate(-1)}>
+								Go back
+							</Button>
+							<Button
+								type="primary"
+								htmlType="submit"
+								loading={mutation.isPending}
+							>
+								Create
+							</Button>
+						</Space>
+					</Form.Item>
+				</Form>
+			</Space>
+		</>
 	);
 };
 
--- src/modules/Department/create.tsx
+++ src/modules/Department/create.tsx
@@ -1,64 +1,106 @@
-import { useAddDepartmentMutation } from "@/services/department.service";
-import { DepartmentRequest } from "@/types/department/DepartmentRequest";
-import { Button, Form, Input, Space } from "antd";
+import departmentService from "@/services/department.service";
+import { DepartmentRequest } from "@/types/department/DepartmentRequest.ts";
+import { useMutation } from "@tanstack/react-query";
+import { Alert, Button, Form, Input, message, Space } from "antd";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const DepartmentCreate = () => {
 	const navigate = useNavigate();
-	const [addDepartment, { isLoading, isSuccess, error, isError, data }] =
-		useAddDepartmentMutation();
+	const [messageApi, contextHolder] = message.useMessage();
+
+	const success = () => {
+		messageApi.open({
+			type: "success",
+			content: "Department added",
+		});
+		setTimeout(messageApi.destroy, 2000);
+	};
+
+	const mutation = useMutation({
+		mutationFn: (newDepartment: DepartmentRequest) =>
+			departmentService.addNew(newDepartment),
+	});
 
 	const createDepartment = (values: DepartmentRequest) => {
-		addDepartment(values);
+		mutation.mutate(values);
 	};
 
 	useEffect(() => {
-		if (isSuccess) {
-			console.log(data);
+		if (mutation.isSuccess) {
+			success();
+			setTimeout(() => {
+				navigate("/admin/department");
+			}, 2500);
 		}
-	}, [isLoading]);
+	}, [mutation]);
 
 	return (
-		<Space direction="vertical" style={{ display: "flex" }} size={"large"}>
-			<h1 style={{ textAlign: "center" }}>Create a department</h1>
-			<Form
-				name="department"
-				labelCol={{ span: 8 }}
-				wrapperCol={{ span: 16 }}
-				style={{ maxWidth: 600 }}
-				onFinish={createDepartment}
+		<>
+			{contextHolder}
+			<Space
+				direction="vertical"
+				style={{ display: "flex" }}
+				size={"large"}
 			>
-				<Form.Item
-					label="Department name"
-					name="name"
-					rules={[
-						{
-							required: true,
-							message: "Please input department name!",
-						},
-					]}
-				>
-					<Input />
-				</Form.Item>
-				<Form.Item label="Department description" name="description">
-					<Input />
-				</Form.Item>
-				<Form.Item>
-					<Space
+				{mutation.isError ? (
+					<Alert
+						message={`Error: ${mutation.error}`}
+						type="warning"
+						closable
 						style={{
-							display: "flex",
+							marginBottom: "20px",
 						}}
-						size={"large"}
+					/>
+				) : null}
+				<h1 style={{ textAlign: "center" }}>Create a department</h1>
+				<Form
+					name="department"
+					labelCol={{ span: 8 }}
+					wrapperCol={{ span: 16 }}
+					style={{ maxWidth: 600 }}
+					onFinish={createDepartment}
+				>
+					<Form.Item
+						label="Department name"
+						name="name"
+						rules={[
+							{
+								required: true,
+								message: "Please input department name!",
+							},
+						]}
 					>
-						<Button onClick={() => navigate(-1)}>Go back</Button>
-						<Button type="primary" htmlType="submit">
-							Create
-						</Button>
-					</Space>
-				</Form.Item>
-			</Form>
-		</Space>
+						<Input />
+					</Form.Item>
+					<Form.Item
+						label="Department description"
+						name="description"
+					>
+						<Input />
+					</Form.Item>
+					<Form.Item>
+						<Space
+							style={{
+								display: "flex",
+							}}
+							size={"large"}
+						>
+							<Button onClick={() => navigate(-1)}>
+								Go back
+							</Button>
+							<Button
+								type="primary"
+								htmlType="submit"
+								loading={mutation.isPending}
+							>
+								Create
+							</Button>
+						</Space>
+					</Form.Item>
+				</Form>
+			</Space>
+		</>
 	);
 };
 
