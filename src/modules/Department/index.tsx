0eec4cf8c5329f613609c31a812dbb4fe0d67f21
rustam <rustamm.aliyev@gmail.com>
2024-04-29 19:00:34 +0400

    add some changed in department table

--- src/modules/Department/index.tsx
+++ src/modules/Department/index.tsx
@@ -1,15 +1,61 @@
-import { useGetDepartmentsQuery } from "@/services/department.service";
-import { DepartmentResponse } from "@/types/department/DepartmentResponse";
-import { Button, Empty, Popconfirm, Space } from "antd";
+import { DepartmentResponse } from "@/types/department/DepartmentResponse.ts";
+import { Alert, Button, Empty, message, Popconfirm, Space } from "antd";
 import PlusOutlined from "@ant-design/icons/lib/icons/PlusOutlined";
 import Table, { ColumnsType } from "antd/es/table";
 import { useNavigate } from "react-router-dom";
+import departmentService from "@/services/department.service.ts";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const Department = () => {
 	const navigate = useNavigate();
-	const { data: departments } = useGetDepartmentsQuery(1);
+	const [messageApi, contextHolder] = message.useMessage();
+
+	const fetchDepartments = async () => {
+		const { data } = await departmentService.getAll();
+		return data;
+	};
+
+	const {
+		isError,
+		error,
+		data: departments,
+	} = useQuery({
+		queryKey: ["departments"],
+		queryFn: () => fetchDepartments(),
+	});
+
+	const success = () => {
+		messageApi.open({
+			type: "success",
+			content: "Department deleted",
+		});
+		setTimeout(messageApi.destroy, 2000);
+	};
+
+	const deleteDepartmentMutation = useMutation({
+		mutationFn: (id: number) => departmentService.delete(id),
+	});
+
+	const deleteDepartment = (id: number) => {
+		deleteDepartmentMutation.mutate(id);
+	};
+
+	useEffect(() => {
+		if (deleteDepartmentMutation.isSuccess) {
+			success();
+			setTimeout(() => {
+				navigate(0);
+			}, 2500);
+		}
+	}, [deleteDepartmentMutation]);
 
 	const columns: ColumnsType<DepartmentResponse> = [
+		{
+			title: "№",
+			key: "index",
+			render: (_, _record, index) => <b>{index + 1}</b>,
+		},
 		{
 			title: "Name",
 			dataIndex: "name",
@@ -28,17 +74,20 @@ const Department = () => {
 					<a
 						key={_}
 						onClick={() => {
-							console.log("aa");
+							navigate(`edit/${record.id}`);
 						}}
 					>
-						Edit {record.name}
+						Edit
 					</a>
 					<Popconfirm
 						title="Delete the department"
 						description="Are you sure to delete this department?"
 						okText="Yes"
 						cancelText="No"
-						onConfirm={() => console.log(record?.id)}
+						onConfirm={() => deleteDepartment(record?.id)}
+						okButtonProps={{
+							loading: deleteDepartmentMutation.isPending,
+						}}
 					>
 						<a key={_ + 1}>Delete</a>
 					</Popconfirm>
@@ -49,6 +98,17 @@ const Department = () => {
 
 	return (
 		<>
+			{contextHolder}
+			{isError ? (
+				<Alert
+					message={`There is some error. ${error}`}
+					type="warning"
+					closable
+					style={{
+						marginBottom: "20px",
+					}}
+				/>
+			) : null}
 			<Space
 				direction="vertical"
 				style={{ display: "flex" }}
